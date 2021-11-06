75a206b02412ca15d79df727812e33d6fc17a658
Manuel Eduardo Sotelo Cervantes <meduardoscervantes@gmail.com>
2021-11-05 19:59:50 -0500

    utilized glassdoor information to generate longitude and latitude locations using google places APi

---
+++ data_cleanse/city_long_lat.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from config import google_places_key
+import pandas as pd
+
+data = pd.read_csv('../resources/glassdoor_data_scientist_jobs.csv')
+sorted_cities = data["Location"].unique()
+
+url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+cities = []
+state = []
+long = []
+lat = []
+for x in sorted_cities:
+    state.append(x[-2:])
+    cities.append(x[:-4])
+    payload = requests.get(f'{url}{str(x).replace(" ", "+")}&key={google_places_key}').json()
+    long.append(payload['results'][0]['geometry']['location']['lng'])
+    lat.append(payload['results'][0]['geometry']['location']['lat'])
+    print(f'{x} added!')
+
+map_info = pd.DataFrame({
+    "city": cities,
+    "state": state,
+    "long": long,
+    "lat": lat
+})
+map_info.to_csv("resources/map_information", index=False)
+
